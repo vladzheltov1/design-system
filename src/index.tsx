6f4f61f347338baabdf9017a748d358605c6a556
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-02-21 21:00:30 +0300

    Fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
 import React, { FC, HTMLAttributes, ReactChild } from 'react';
-import style from './style.module.css';
+import style from './style.module.scss';
 
 export interface Props extends HTMLAttributes<HTMLDivElement> {
-  /** custom content, defaults to 'the snozzberries taste like snozzberries' */
-  children?: ReactChild;
+    children?: ReactChild;
 }
 
 // Please do not use types off of a default export module or else Storybook Docs will suffer.
 // see: https://github.com/storybookjs/storybook/issues/9556
-/**
- * A custom Thing component. Neat!
- */
 export const Thing: FC<Props> = ({ children }) => {
-  return <div className={style.div}>{children || `the snozzberries taste like snozzberries`}</div>;
+    return <div className={style.div}>{children || `the snozzberries taste like snozzberries`}</div>;
 };
